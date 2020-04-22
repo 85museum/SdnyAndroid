cd705cff6d4a62e153b7073f3e66c1ea33673f82
85museum <2528499442@qq.com>
2020-04-22 15:21:35 +0800

    20204.22

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180115/Ch10Activity02.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180115/Ch10Activity02.java
@@ -1,9 +1,12 @@
 package cn.edu.sdwu.android02.classroom.sn170507180115;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Ch10Activity02 extends AppCompatActivity {
 
@@ -19,5 +22,37 @@ public class Ch10Activity02 extends AppCompatActivity {
         intent.putExtra("key1","message");
 
         sendBroadcast(intent);
+    }
+    public void ch10Activity1(View view){
+        Intent intent=new Intent(this,Ch10Activity1.class);
+        EditText editText=(EditText)findViewById(R.id.ch10_2_et);
+        intent.putExtra("text",editText.getText().toString());//设置传递的顺序
+        startActivity(intent);
+    }
+
+    public void startSubActivity(View view){
+        //
+        Intent intent=new Intent(this,Ch10Activity3.class);
+        startActivityForResult(intent,101);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //在父Activity中获取返回值
+        //requestCode用来区分从哪一个子activity返回的结果
+        if(requestCode==101){
+            if(resultCode==RESULT_OK){
+                String name=data.getStringExtra("name");
+
+                Toast.makeText(this,name,Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(this,"cancel",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void web(View view){
+        //使用隐式启动方式
+        Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://baidu.com"));
+        startActivity(intent);
     }
 }
--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180115/MainActivity.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180115/MainActivity.java
@@ -2,6 +2,7 @@ package cn.edu.sdwu.android02.classroom.sn170507180115;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void startSubActivity(View v){
+
     }
 }
